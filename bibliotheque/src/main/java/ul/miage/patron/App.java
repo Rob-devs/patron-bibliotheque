c1404a5f95498a430d3709832d2cec60f7b80d80
Rob <robbrumbter@pm.me>
2023-10-14 10:28:45 +0200

    Adding maven project structure

---
+++ bibliotheque/src/main/java/ul/miage/patron/App.java
@@ -0,0 +1,13 @@
+package ul.miage.patron;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
