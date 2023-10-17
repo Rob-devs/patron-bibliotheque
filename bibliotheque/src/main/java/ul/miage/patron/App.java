e085853dd00f123439b793c583140e5841e8728c
Evequios <kilianbnr@outlook.fr>
2023-10-17 10:26:21 +0200

    Configuration de Java FX

--- bibliotheque/src/main/java/ul/miage/patron/App.java
+++ bibliotheque/src/main/java/ul/miage/patron/App.java
@@ -1,13 +1,33 @@
 package ul.miage.patron;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
+	private static Scene scene;
+	
+	@Override
+	public void start(Stage stage) throws IOException{
+		scene = new Scene(loadFXML("/TestApp"));
+		stage.setScene(scene);
+		stage.show();
+		stage.setResizable(false);
+	}
+	
+	static void setRoot(String fxml) throws IOException{
+		scene.setRoot(loadFXML(fxml));
+	}
+	
+	private static Parent loadFXML(String fxml) throws IOException{
+		FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml+".fxml"));
+		return fxmlLoader.load();
+	}
+	public static void main(String[] args) {
+		launch();
+	}
 }
