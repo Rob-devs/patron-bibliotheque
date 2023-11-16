1cf4fc3e9de29fce4849d906bfb1b28d9e74873d
Evequios <kilianbnr@outlook.fr>
2023-11-16 13:15:51 +0100

    Ajout Date

--- bibliotheque/src/main/java/ul/miage/patron/utilities/Date.java
+++ bibliotheque/src/main/java/ul/miage/patron/utilities/Date.java
@@ -4,6 +4,13 @@ public class Date {
     private int jour;
     private int mois;
     private int annee;
+
+    public Date(int jour, int mois, int annee){
+        this.jour = jour;
+        this.mois = mois;
+        this.annee = annee;
+    }
+
     public int getJour() {
         return jour;
     }
